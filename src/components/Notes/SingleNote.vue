5d6c8017587e5b1f617d3b883864fd4ef25685a1
Marzia Yarzadah <marziayarzadah32@gmail.com>
2024-04-06 08:46:23 +0430

    add a singleNote page as child component

---
+++ src/components/Notes/SingleNote.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+});
+const emit=defineEmits(['onDeleteNote'])
+const characterLength = computed(() => {
+  const noteLength = props.note.content.length;
+  const des=noteLength > 1 ?'characters' :'character'
+  return `${props.note.content.length}  ${des}`;
+});
+
+const onDeleteNote = () => {
+  emit('onDeleteNote',props.note.id)
+}
+</script>
+<template>
+  <div class="card-content">
+    <div class="content">
+      {{ note.content }}
+      <div class="has-text-right">
+        <small>{{ characterLength }} </small>
+      </div>
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">Edit</a>
+    <a href="#" class="card-footer-item" @click.prevent="onDeleteNote">Delete</a>
+  </footer>
+</template>
